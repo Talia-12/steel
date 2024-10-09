95034cc2b9befa4e80e18be253a10b421abd35b5
Talia-12 <paradox.m.burgess@gmail.com>
2024-10-09 22:35:24 +1000

    fixing errors making not compile in helix

--- flake.nix
+++ flake.nix
@@ -4,15 +4,27 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    rust-overlay
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+
       fs = pkgs.lib.fileset;
 
       manifest = pkgs.lib.importTOML ./Cargo.toml;
